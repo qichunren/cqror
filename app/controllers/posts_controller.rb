f16a19ab9e699f5ebf0cffb1567be9c60d838115
qichunren <qichunren@usermatoMacBook-Pro.local>
2010-10-23 16:12:17 +0800

    increment click_count for post #show method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,9 @@ class PostsController < ApplicationController
   # GET /posts/1.xml
   def show
     @post = Post.find(params[:id])
-
+    @post.click_count += 1
+    @post.save
+    
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @post }
